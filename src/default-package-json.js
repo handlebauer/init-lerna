fa7d7ce3f0f92e8e6ca4308d0581e9c3b92eed64
Donald Geddes <hi@donaldgeddes.ca>
2022-02-10 22:49:02 -0800

    Add support for basic prackage scripts

--- src/default-package-json.js
+++ src/default-package-json.js
@@ -1,4 +1,9 @@
 export const defaultPackageJson = {
   name: 'root',
   private: true,
+  scripts: {
+    test: 'lerna run test',
+    build: 'lerna run build',
+    version: 'lerna run version',
+  },
 }
